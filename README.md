66cac33656d6ef36ced8259f53d436b2e7ac5912
Chris Bowman <stereoabusestudios@gmail.com>
2020-11-30 22:16:53 -0500

    More functions in utilities
    website interaction

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Advent of Code 2020
 
-Goals for this year include learning more about git in the process.
-
-
+Goals for this year include learning git and PyCharm.
--- advent_utils.py
+++ advent_utils.py
@@ -1,19 +1,47 @@
 import re
+import requests
+from bs4 import BeautifulSoup
+import config   # AoC website login
 
 
-def integers(s: str) -> tuple:
-    """Takes a string and return just digits split into tuple"""
-    return tuple(int(i) for i in re.split(r'\D+', s) if i)
+# Web interaction
+def web_input(day, year=2020):
+    """Snags input data from AoC website, returns string."""
+    url = f'https://adventofcode.com/{year}/day/{day}/input'
+    return requests.get(url, cookies=config.COOKIES).text
 
 
-def mapl(s: str) -> list:
-    """Takes a string and return just digits split into list"""
-    return [int(i) for i in re.split(r'\D+', s) if i]
+def local_input(day):
+    """Takes input data from local file, returns string or FileNotFoundError."""
+    file = f'input/{str(day).zfill(2)}'
+    try:
+        return open(file, 'r').read()
+    except FileNotFoundError as error:
+        return error
 
 
-def taxi_distance(x,y=(0,0)):
-    """Return integer of Manhattan distance of 2 points"""
+def gen_local_input(day, year=2020):
+    """Creates local file of the day's input data."""
+    with open(f'inputs/{str(day).zfill(2)}', 'w') as file:
+        file.write(web_input(day, year))
+
+
+def submit(answer, day, part, year=2020):
+    """Submits my answer to the AoC website and returns if accepted or not."""
+    url = f'https://adventofcode.com/{year}/day/{day}/input'
+    form = {'level': str(part), 'answer': str(answer)}
+    r = requests.post(url, cookies=config.COOKIES, data=form)
+    soup = BeautifulSoup(r.text, features='html.parser')
+    reply = soup.find('p').text
+    return reply
+
+
+# Parsing inputs
+def integers(s):
+    """Takes a string and return digits split by any other character into generator."""
+    return (int(i) for i in re.split(r'\D+', s) if i)
+
+
+def taxi_distance(x, y=(0, 0)):
+    """Return integer of Manhattan distance of 2 points."""
     return abs(x[0] - x[1]) + abs(y[0] - y[1])
-
-
-print(integers('sdgfsdh2332n2234n5655n'))
