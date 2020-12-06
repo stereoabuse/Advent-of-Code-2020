27424ac7d374263dd2eb909d6c8492f9ee43ac7d
Chris Bowman <stereoabusestudios@gmail.com>
2020-12-06 01:54:57 -0500

    type hint: day 06

--- solutions/day_06.py
+++ solutions/day_06.py
@@ -6,17 +6,19 @@
 """
 
 
-def part_1(data):
+def part_1(data: list) -> int:
+    """Takes list of tuples and returns the total sum of unique values in each item"""
     return sum(len(set(i[0])) for i in data)
 
 
-def part_2(data):
+def part_2(data: list) -> int:
+    """Takes list of tuples and returns the sum of sum of letters who appear in each line of a data sub-item"""
     return sum(sum(1 for let in set(item[0]) if item[0].count(let) == item[1]) for item in data)
 
 
 def main():
-    d = open('../inputs/06').read()
-    d = [(i.replace('\n', ''), i.count('\n') + 1) for i in d.split('\n\n')]
+    d = open('../inputs/06').read().split('\n\n')
+    d = [(i.replace('\n', ''), i.count('\n') + 1) for i in d]
     print(part_1(d))
     print(part_2(d))
 
