bfcc1e0dcdddf5d7c11051a71d939c3d3e566c31
Chris Bowman <stereoabusestudios@gmail.com>
2020-12-09 00:53:17 -0500

    day9

---
+++ solutions/day_09.py
@@ -0,0 +1,35 @@
+""" day_09.py -- Advent of Code 2020 Day 9:
+
+    Author: Chris Bowman
+    Last Modified: 12/9/2020
+    License: MIT
+"""
+
+from itertools import combinations
+from utils import integers
+
+
+def xmas(preamble, data):
+    for index, _ in enumerate(data):
+        if index > preamble:
+            if not any(sum(i) == data[index] for i in combinations(data[index - preamble:index], 2)):
+                return data[index]
+
+
+def part_2(preamble, data):
+    ans = xmas(preamble, data)
+    target = data.index(xmas(preamble, data))
+    for x in range(len(data[:target])):
+        for y in range(x):
+            if sum(data[y:x]) == ans:
+                return min(data[y:x]) + max(data[y:x])
+
+
+def main():
+    d = integers(open('../inputs/09').read())
+    print(xmas(25, d))
+    print(part_2(25, d))
+
+
+if __name__ == '__main__':
+    main()
